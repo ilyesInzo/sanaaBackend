24ab1446ba1fe1512f3023262486026de2548586
ilyesInzo <ilyes.mansour.3@gmail.com>
2021-06-20 19:42:29 +0200

    init project

---
+++ src/main/java/com/magma/sanaa/SanaaApplication.java
@@ -0,0 +1,13 @@
+package com.magma.sanaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanaaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magma/sanaa/SanaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magma.sanaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
